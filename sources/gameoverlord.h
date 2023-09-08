9d6e23554ce0fa235d679854f9403be33188d591
ybtre <vuchev7892@gmail.com>
2023-09-08 09:46:35 +0300

    update

--- sources/gameoverlord.h
+++ sources/gameoverlord.h
@@ -3,59 +3,45 @@
 
 #define ARR_WAVES 10
 
-typedef struct AWave{
-	int level;
-	float duration;
-	float enemy_spawn_interval;
+typedef struct AWave {
+  int level;
+  float duration;
+  float enemy_spawn_interval;
 } AWave;
 
-class GameOverlord
-{
+class GameOverlord {
 public:
-	enum SCREENS {
-		MAIN_MENU 	= 0,
-		GAMEPLAY 	= 1,
-		GAME_OVER 	= 2,
-		UPGRADE 	= 3
-	};
+  enum SCREENS { MAIN_MENU = 0, GAMEPLAY = 1, GAME_OVER = 2, UPGRADE = 3 };
 
-	GameOverlord();
+  GameOverlord();
 
-	void SetupGame();
-	bool IsGameOver();
-	void ResetGame();
+  void SetupGame();
+  bool IsGameOver();
+  void ResetGame();
 
-	SCREENS GetCurrentScreen();
-	void SetCurrentScreen(SCREENS new_screen);
+  SCREENS GetCurrentScreen();
+  void SetCurrentScreen(SCREENS new_screen);
 
-	SCREENS current_screen{};
+  SCREENS current_screen{};
 
-	int score{};
+  int score{};
 
-	bool is_paused = false;
-	int pause_blink_counter = 0;
+  bool is_paused = false;
+  int pause_blink_counter = 0;
 
-	int current_wave = 0;
-	float gameplay_time_total = 0;
-	float gameplay_time_current = 0;
-	float wave_duration = 0.f;
-	bool is_wave_starting = false;
-	// bool has_wave_finished = false;
-	AWave waves[ARR_WAVES] = {
-		{ 1, 10.9f, 1.f },
-		{ 2, 12.9f, .8f },
-		{ 3, 14.9f, .7f },
-		{ 4, 16.9f, .65f },
-		{ 5, 20.9f, .625f },
-		{ 6, 25.9f, .6f },
-		{ 7, 35.9f, .55f },
-		{ 8, 40.9f, .5f },
-		{ 9, 50.9f, .4f },
-		{ 10, 60.9f, .2f }
-	};
+  int current_wave = 0;
+  float gameplay_time_total = 0;
+  float gameplay_time_current = 0;
+  float wave_duration = 0.f;
+  bool is_wave_starting = false;
+  // bool has_wave_finished = false;
+  AWave waves[ARR_WAVES] = {
+      {1, 10.9f, 1.f},   {2, 12.9f, .8f}, {3, 14.9f, .7f},  {4, 16.9f, .65f},
+      {5, 20.9f, .625f}, {6, 25.9f, .6f}, {7, 35.9f, .55f}, {8, 40.9f, .5f},
+      {9, 50.9f, .4f},   {10, 60.9f, .2f}};
 
-	Rectangle live_src;
-	Rectangle live_dest;
-	Rectangle score_src;
-	Rectangle score_dest;
+  Rectangle live_src;
+  Rectangle live_dest;
+  Rectangle score_src;
+  Rectangle score_dest;
 };
--- sources/player.cpp
+++ sources/player.cpp
@@ -1,151 +1,126 @@
 #include "entities.h"
 
-void SetupPlayer(Player& player) {
-	player.e.is_alive = true;
-	player.stats.lives = 3;
-	player.stats.fire_rate = .4f;
-	player.stats.fire_rate_timer = 0;
-	player.stats.xp_pickup_range = 50;
-	player.stats.required_xp = 5;
-	player.rotation = 0;
+void SetupPlayer(Player &player) {
+  player.e.is_alive = true;
+  player.stats.lives = 3;
+  player.stats.fire_rate = .4f;
+  player.stats.fire_rate_timer = 0;
+  player.stats.xp_pickup_range = 50;
+  player.stats.required_xp = 5;
+  player.rotation = 0;
 
-	player.e.rec = { (GLOBALS::SCREEN.x / 2), (GLOBALS::SCREEN.y / 2), SPRITE_SIZE_SCALED, SPRITE_SIZE_SCALED };
-	player.e.sprite.src = { 56, 0, SPRITE_SIZE, SPRITE_SIZE };
-	player.e.sprite.dest = {
-		player.e.rec.x + SPRITE_OFFSET,
-		player.e.rec.y + SPRITE_OFFSET,
-		player.e.sprite.src.width * SPRITE_SCALE_MULTI,
-		player.e.sprite.src.height * SPRITE_SCALE_MULTI
-	};
-	player.e.sprite.center = {
-		(player.e.sprite.src.width * SPRITE_SCALE_MULTI) / 2,
-		(player.e.sprite.src.height * SPRITE_SCALE_MULTI) / 2
-	};
+  player.e.rec = {(GLOBALS::SCREEN.x / 2), (GLOBALS::SCREEN.y / 2),
+                  SPRITE_SIZE_SCALED, SPRITE_SIZE_SCALED};
+  player.e.sprite.src = {56, 0, SPRITE_SIZE, SPRITE_SIZE};
+  player.e.sprite.dest = {player.e.rec.x + SPRITE_OFFSET,
+                          player.e.rec.y + SPRITE_OFFSET,
+                          player.e.sprite.src.width * SPRITE_SCALE_MULTI,
+                          player.e.sprite.src.height * SPRITE_SCALE_MULTI};
+  player.e.sprite.center = {
+      (player.e.sprite.src.width * SPRITE_SCALE_MULTI) / 2,
+      (player.e.sprite.src.height * SPRITE_SCALE_MULTI) / 2};
 
-	player.engine_s.src = { 72, 0, SPRITE_SIZE, SPRITE_SIZE * 2 };
-	player.engine_s.dest = {
-		player.e.sprite.dest.x,
-		player.e.sprite.dest.y,
-		player.e.sprite.dest.width,
-		player.e.sprite.dest.height * 2
-	};
-	player.engine_s.center = {
-		player.e.sprite.center.x,
-		player.e.sprite.center.y
-	};
+  player.engine_s.src = {72, 0, SPRITE_SIZE, SPRITE_SIZE * 2};
+  player.engine_s.dest = {player.e.sprite.dest.x, player.e.sprite.dest.y,
+                          player.e.sprite.dest.width,
+                          player.e.sprite.dest.height * 2};
+  player.engine_s.center = {player.e.sprite.center.x, player.e.sprite.center.y};
 
-	player.engine_anim.frames_counter = 0;
-	player.engine_anim.current_frame = 0;
-	player.engine_anim.num_frames = 4;
-	player.engine_anim.is_active = false;
+  player.engine_anim.frames_counter = 0;
+  player.engine_anim.current_frame = 0;
+  player.engine_anim.num_frames = 4;
+  player.engine_anim.is_active = false;
 
-	player.e.color = C_PLAYER;
-	player.e.speed = 110;
+  player.e.color = C_PLAYER;
+  player.e.speed = 110;
 };
 
-void ResetPlayer_OnDie(Player& player) {
-	player.e.rec = { GLOBALS::SCREEN.x / 2, GLOBALS::SCREEN.y / 2, SPRITE_SIZE_SCALED, SPRITE_SIZE_SCALED };
-	player.e.sprite.dest = {
-		player.e.rec.x + SPRITE_OFFSET,
-		player.e.rec.y + SPRITE_OFFSET,
-		player.e.sprite.src.width * SPRITE_SCALE_MULTI,
-		player.e.sprite.src.height * SPRITE_SCALE_MULTI
-	};
-	player.engine_s.dest = {
-		player.e.sprite.dest.x,
-		player.e.sprite.dest.y,
-		player.e.sprite.dest.width,
-		player.e.sprite.dest.height * 2
-	};
+void ResetPlayer_OnDie(Player &player) {
+  player.e.rec = {GLOBALS::SCREEN.x / 2, GLOBALS::SCREEN.y / 2,
+                  SPRITE_SIZE_SCALED, SPRITE_SIZE_SCALED};
+  player.e.sprite.dest = {player.e.rec.x + SPRITE_OFFSET,
+                          player.e.rec.y + SPRITE_OFFSET,
+                          player.e.sprite.src.width * SPRITE_SCALE_MULTI,
+                          player.e.sprite.src.height * SPRITE_SCALE_MULTI};
+  player.engine_s.dest = {player.e.sprite.dest.x, player.e.sprite.dest.y,
+                          player.e.sprite.dest.width,
+                          player.e.sprite.dest.height * 2};
 };
 
-void ResetPlayer_OnGameOver(Player& player) {
-	player.e.is_alive = true;
-	player.stats.lives = 3;
-	player.e.rec = { GLOBALS::SCREEN.x / 2, GLOBALS::SCREEN.y / 2, SPRITE_SIZE_SCALED, SPRITE_SIZE_SCALED };
-	player.e.sprite.dest = {
-		player.e.rec.x + SPRITE_OFFSET,
-		player.e.rec.y + SPRITE_OFFSET,
-		player.e.sprite.src.width * SPRITE_SCALE_MULTI,
-		player.e.sprite.src.height * SPRITE_SCALE_MULTI
-	};
-	player.engine_s.dest = {
-		player.e.sprite.dest.x,
-		player.e.sprite.dest.y,
-		player.e.sprite.dest.width,
-		player.e.sprite.dest.height * 2
-	};
+void ResetPlayer_OnGameOver(Player &player) {
+  player.e.is_alive = true;
+  player.stats.lives = 3;
+  player.e.rec = {GLOBALS::SCREEN.x / 2, GLOBALS::SCREEN.y / 2,
+                  SPRITE_SIZE_SCALED, SPRITE_SIZE_SCALED};
+  player.e.sprite.dest = {player.e.rec.x + SPRITE_OFFSET,
+                          player.e.rec.y + SPRITE_OFFSET,
+                          player.e.sprite.src.width * SPRITE_SCALE_MULTI,
+                          player.e.sprite.src.height * SPRITE_SCALE_MULTI};
+  player.engine_s.dest = {player.e.sprite.dest.x, player.e.sprite.dest.y,
+                          player.e.sprite.dest.width,
+                          player.e.sprite.dest.height * 2};
 };
 
-void UpdatePlayer(Player& player, Sprite &bg) {
+void UpdatePlayer(Player &player, Sprite &bg) {
 
-	player.stats.fire_rate_timer -= GetFrameTime();
+  player.stats.fire_rate_timer -= GetFrameTime();
 
-	//------Player movement------//
-	player.engine_anim.is_active = true;
-	if (IsKeyDown(KEY_W)) {
-		player.e.rec.y -= player.e.speed * GetFrameTime();
-		player.e.sprite.dest.y -= player.e.speed * GetFrameTime();
-		bg.dest.y += player.e.speed * GetFrameTime() * .1f ;
-	}
-	if (IsKeyDown(KEY_S)) {
-		player.e.rec.y += player.e.speed* GetFrameTime();
-		player.e.sprite.dest.y += player.e.speed* GetFrameTime();
-		bg.dest.y -= player.e.speed * GetFrameTime() * .1f ;
-	}
-	if (IsKeyDown(KEY_A)) {
-		player.e.rec.x -= player.e.speed* GetFrameTime();
-		player.e.sprite.dest.x -= player.e.speed* GetFrameTime();
-		bg.dest.x += player.e.speed * GetFrameTime() * .1f ;
-	}
-	if (IsKeyDown(KEY_D)) {
-		player.e.rec.x += player.e.speed* GetFrameTime();
-		player.e.sprite.dest.x += player.e.speed* GetFrameTime();
-		bg.dest.x -= player.e.speed * GetFrameTime() * .1f ;
-	}
+  //------Player movement------//
+  player.engine_anim.is_active = true;
+  if (IsKeyDown(KEY_W)) {
+    player.e.rec.y -= player.e.speed * GetFrameTime();
+    player.e.sprite.dest.y -= player.e.speed * GetFrameTime();
+    bg.dest.y += player.e.speed * GetFrameTime() * .1f;
+  }
+  if (IsKeyDown(KEY_S)) {
+    player.e.rec.y += player.e.speed * GetFrameTime();
+    player.e.sprite.dest.y += player.e.speed * GetFrameTime();
+    bg.dest.y -= player.e.speed * GetFrameTime() * .1f;
+  }
+  if (IsKeyDown(KEY_A)) {
+    player.e.rec.x -= player.e.speed * GetFrameTime();
+    player.e.sprite.dest.x -= player.e.speed * GetFrameTime();
+    bg.dest.x += player.e.speed * GetFrameTime() * .1f;
+  }
+  if (IsKeyDown(KEY_D)) {
+    player.e.rec.x += player.e.speed * GetFrameTime();
+    player.e.sprite.dest.x += player.e.speed * GetFrameTime();
+    bg.dest.x -= player.e.speed * GetFrameTime() * .1f;
+  }
 
-	float dx = player.e.rec.x - (GetMouseX() - SPRITE_OFFSET);
-	float dy = player.e.rec.y - (GetMouseY() - SPRITE_OFFSET);
-	float final_rotation = ((atan2(dy, dx)) * 180 / PI) - 90;
+  float dx = player.e.rec.x - (GetMouseX() - SPRITE_OFFSET);
+  float dy = player.e.rec.y - (GetMouseY() - SPRITE_OFFSET);
+  float final_rotation = ((atan2(dy, dx)) * 180 / PI) - 90;
 
-	player.rotation = final_rotation;
+  player.rotation = final_rotation;
 
-	player.engine_s.dest.x = player.e.sprite.dest.x;
-	player.engine_s.dest.y = player.e.sprite.dest.y;
-	UpdateAnimation(player.engine_anim, true);
-	player.engine_s.src.x = (float)(72 + (SPRITE_SIZE * player.engine_anim.current_frame));
+  player.engine_s.dest.x = player.e.sprite.dest.x;
+  player.engine_s.dest.y = player.e.sprite.dest.y;
+  UpdateAnimation(player.engine_anim, true);
+  player.engine_s.src.x =
+      (float)(72 + (SPRITE_SIZE * player.engine_anim.current_frame));
 };
 
-void RenderPlayer(const Player& player, const Texture2D& game_atlas) {
+void RenderPlayer(const Player &player, const Texture2D &game_atlas) {
 #ifdef DEBUGGING
-	DrawRectangleLinesEx(player.e.rec, 1, player.e.color);
-	DrawCircleLines(player.e.rec.x, player.e.rec.y, 10, player.e.color);
+  DrawRectangleLinesEx(player.e.rec, 1, player.e.color);
+  DrawCircleLines(player.e.rec.x, player.e.rec.y, 10, player.e.color);
 #endif
-	if (player.engine_anim.is_active) {
-		DrawTexturePro(game_atlas,
-		               player.engine_s.src,
-		               player.engine_s.dest,
-		               player.engine_s.center,
-		               player.rotation,
-		               WHITE);
-	}
+  if (player.engine_anim.is_active) {
+    DrawTexturePro(game_atlas, player.engine_s.src, player.engine_s.dest,
+                   player.engine_s.center, player.rotation, WHITE);
+  }
 
-	DrawTexturePro(game_atlas,
-	               player.e.sprite.src,
-	               player.e.sprite.dest,
-	               player.e.sprite.center,
-	               player.rotation,
-	               WHITE);
+  DrawTexturePro(game_atlas, player.e.sprite.src, player.e.sprite.dest,
+                 player.e.sprite.center, player.rotation, WHITE);
 };
 
-void IncreaseReqXP(Player &player){
-	player.stats.required_xp *= 1.2f;
+void IncreaseReqXP(Player &player) { player.stats.required_xp *= 1.2f; }
+
+void Upgrade_PlayerFirerate(Player &player, float multiplier) {
+  player.stats.fire_rate /= multiplier;
 }
 
-void Upgrade_PlayerFirerate(Player &player, float multiplier){
-	player.stats.fire_rate /= multiplier;
-}
-
-void Upgrade_PlayerSpeed(Player &player, float multiplier){
-	player.e.speed *= multiplier;
+void Upgrade_PlayerSpeed(Player &player, float multiplier) {
+  player.e.speed *= multiplier;
 }
